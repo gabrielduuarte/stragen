ce6a8aee6ada79a234c6825842424e4616b18741
Gabriel De Andrade Duarte <duarte0904@gmail.com>
2018-11-15 10:09:51 -0300

    os dados agora estao normalizados de 0 a 1 segundo a tese

--- stragen.c
+++ stragen.c
@@ -12,8 +12,12 @@
     #define DEBUG 0
 #endif
 
-
+/*prototipos*/
 void entrada(float b[LIN][DIM]);
+void normaliza_entrada(float b[LIN][DIM], float w[LIN][DIM]);
+float minmax(float b[LIN][DIM], int j, char vez);
+
+/*************/
 
 int main(void)
 {
@@ -21,7 +25,56 @@ int main(void)
 
     entrada(b);
 
+    normaliza_entrada(b, w);
+
     return EXIT_SUCCESS;
+}
+
+void normaliza_entrada(float b[LIN][DIM], float w[LIN][DIM])
+{
+    int i, j;
+    float max, min;
+
+    for(j=0; j<DIM; j++)
+    {
+        min = minmax(b, j, 'm');
+        max = minmax(b, j, 'M');
+
+        if(DEBUG) printf("min:%f | max:%f\n", min, max);
+
+        for(i=0; i<LIN; i++)
+            w[i][j] = (b[i][j] - min)/(max - min);
+    }
+
+    if(DEBUG)
+    {
+        for(i=0; i<LIN; i++)
+        {
+            for(j=0; j<DIM; j++) 
+                printf("w[%d][%d]: %.2f\n", i, j, w[i][j]);
+            printf("\n");
+        }
+    }
+
+    return;
+}
+
+float minmax(float b[LIN][DIM], int j, char vez)
+{
+    int i;
+    float minimax;
+
+    minimax = b[0][j];
+    for(i=0; i<LIN; i++)
+    {
+        if(vez == 'm' && b[i][j] < minimax)
+            minimax = b[i][j];
+        
+        if(vez == 'M' && b[i][j] > minimax)
+            minimax = b[i][j];
+    }
+
+    return minimax;
 }
 
 void entrada(float b[LIN][DIM])
