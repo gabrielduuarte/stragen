6d27871882af4bdfbf9e975a6bb57b9a368004e3
Gabriel De Andrade Duarte <duarte0904@gmail.com>
2018-11-15 09:44:48 -0300

    add stragen.c

---
+++ stragen.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define LIN 600
+#define DIM 6
+
+
+int main(void)
+{
+    float b[LIN][DIM]={}, w[LIN][DIM]={};
+
+
+
+    return EXIT_SUCCESS;
+}
+
